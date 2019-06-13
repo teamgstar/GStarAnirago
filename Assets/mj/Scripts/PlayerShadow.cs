34203c985c3a82227a90c64d7e2498a4dc50aba4
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2019-06-13 20:36:45 +0900

    잉?

--- Assets/mj/Scripts/PlayerShadow.cs
+++ Assets/mj/Scripts/PlayerShadow.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerShadow : MonoBehaviour
 {
-    public void Awake(Sprite ShadowSprite)
-    {
-        this.GetComponent<SpriteRenderer>().sprite = ShadowSprite;
-    }
+    //public void Awake(Sprite ShadowSprite)
+    //{
+    //    this.GetComponent<SpriteRenderer>().sprite = ShadowSprite;
+    //}
 
     // Start is called before the first frame update
     void Start()
