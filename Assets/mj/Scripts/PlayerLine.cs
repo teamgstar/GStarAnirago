4cdb9c08ffc3dc879057492a5b2d8fb275ed985a
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2019-06-13 18:00:38 +0900

    다시 시작ㅎㅎㅋㅋㅋ......ㅋㅋㅋ

---
+++ Assets/mj/Scripts/PlayerLine.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLine : MonoBehaviour
+{
+    public  float MaxRange;
+
+    public GameObject line;
+    public GameObject ClickLine;
+    private LineRenderer m_LineRenderer;
+
+    private GameObject m_Player;
+    private SpriteRenderer m_Render;
+
+    private PlayerMovement m_PlayerMovement;
+
+    // Use this for initialization
+
+    private void Awake()
+    {
+        m_LineRenderer      = ClickLine.GetComponent<LineRenderer>();
+        m_Render            = line.GetComponent<SpriteRenderer>();
+        m_Player = GameObject.FindWithTag("Player");
+        m_PlayerMovement = m_Player.GetComponent<PlayerMovement>();
+
+        //라인렌더러 설정
+
+    }
+    void Start()
+    {
+        //시작 컬러값과 나중 컬러값 설정, 이러면 서서히 색이 바뀌게 된다.
+        m_LineRenderer.startColor = Color.red;
+        m_LineRenderer.endColor = Color.yellow;
+
+        //시작 넓이와 나중 넓이 설정, 이러면 서서히 넓이가 바뀌게 된다.
+        m_LineRenderer.startWidth = 0.25f;
+        m_LineRenderer.endWidth = 0.0f;
+        
+        //터치 스크롤시에만 렌더링 될 것 이므로 일단은 기능 꺼놓기
+        m_LineRenderer.enabled = false;
+        m_Render.enabled = false;
+
+        //플레이어 오브젝트를 찾는다
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Press 상태일경우
+        if (m_PlayerMovement.m_Status == PlayerMovement.PlayerStatus.PS_Press)
+        {
+            //플레이어 기준 라인이므로 좌표값을 플레이어의 좌표값으로 설정해줘야한다.
+            this.transform.position = m_Player.transform.position;
+
+            //터지 기준 라인이므로 처음 마우스 좌표값 에서 나중 마우스 좌표값 까지 선을 이어준다.
+            m_LineRenderer.SetPosition(1, new Vector3(m_PlayerMovement.m_StartPos.x, m_PlayerMovement.m_StartPos.y, -1));
+            m_LineRenderer.SetPosition(0, new Vector3(m_PlayerMovement.m_EndPos.x, m_PlayerMovement.m_EndPos.y, -1));
+
+            //렌더러 기능들 모두 켜준다.
+            m_LineRenderer.enabled = true;
+            m_Render.enabled = true;
+
+        }
+        else
+        {
+            //이어진 선을 모두 풀어주고
+            m_LineRenderer.SetPosition(1, Vector3.zero);
+            m_LineRenderer.SetPosition(0, Vector3.zero);
+
+            //렌더러 기능들 모두 꺼준다.
+            m_LineRenderer.enabled = false;
+            m_Render.enabled = false;
+        }
+
+        //처음좌표값과 나중좌표값의 각도 (아크탄젠트)를 구하고
+        float Rot = Mathf.Atan2(-m_PlayerMovement.m_Direction.y, -m_PlayerMovement.m_Direction.x) * 180 / Mathf.PI;
+
+        //플레이어 기준 라인을 회전시켜준다.
+        this.transform.rotation =  Quaternion.Euler(0, 0, Rot+ 90);
+
+        //두 점사이의 거리를 구해서
+        float distance = Mathf.Abs(Vector2.Distance(m_PlayerMovement.m_EndPos, m_PlayerMovement.m_StartPos));
+
+
+        //최대 거리보다 길 경우 설정해준다.
+        if (distance   > MaxRange)
+        { distance = MaxRange; }
+
+        //선의 길이도 늘려준다.
+        this.transform.localScale = new Vector3(1, distance, 1);
+    }
+}
---
+++ Assets/mj/Scripts/PlayerMovement.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    [Tooltip("초당 해당 이동속도의 100 배수 만큼 이동합니다.")]
+    public float MoveSpeed;
+
+
+
+    [HideInInspector] public enum PlayerStatus : byte
+    {
+        PS_Idle, //아무것도 안누르고 있을 때
+        PS_Press, //누르고 있을 때
+    }
+    [HideInInspector] public PlayerStatus m_Status;
+
+    [HideInInspector] public Vector2 m_Direction;
+
+    [HideInInspector] public Vector2 m_StartPos;
+    [HideInInspector] public Vector2 m_EndPos;
+
+    [HideInInspector] public Animator m_Anime;
+    [HideInInspector] public Rigidbody2D m_Rigid;
+
+    [HideInInspector] public enum CollDir
+    {
+        CD_Left,
+        CD_Right,
+        CD_Top,
+        CD_Bottom
+    }
+
+    [HideInInspector] public CollDir m_CollDir;
+    
+    private Camera m_Camera;
+    private SpriteRenderer m_Renderer;
+
+    private void Awake()
+    {
+        m_Direction = Vector2.zero;
+
+        m_StartPos  = Vector2.zero;
+        m_EndPos    = Vector2.zero;
+
+        m_Status    = PlayerStatus.PS_Idle;
+ 
+    }
+
+    void Start()
+    {
+        m_Camera    = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        m_Anime     = GetComponentInChildren<Animator>();
+        m_Renderer  = GetComponentInChildren<SpriteRenderer>();
+        m_Rigid     = this.GetComponentInParent<Rigidbody2D>();
+
+        //애니메이션 상태 초기화
+        m_Anime.SetBool("b_Move", false);
+    }
+
+    private void Update()
+    {
+        switch (m_Status)
+        {
+            case PlayerStatus.PS_Idle:
+                //화면 터치 다운시에
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    //게임속도 느리게해주고
+                    GameManager.g_GameSpeed = 0.025f;
+
+                    //처음 좌표값 설정
+                    m_StartPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                    //기존 Velocity값에 게임속도값을 곱해서 기존 날라가고있는경우에 감속
+                    m_Rigid.velocity *= GameManager.g_GameSpeed;
+
+                    //열거형 값을 Press상태로 변경
+                    m_Status = PlayerStatus.PS_Press;
+                }
+                break;
+            case PlayerStatus.PS_Press:
+                //EndPos를 항상 커서좌표로 갱신해줘서 라인 좌표값도 갱신되게 해줌
+                m_EndPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                
+                //방향벡터 구하고 아크탄젠트 구해서 버튼 터치 다운 좌표값과 터치 업 좌표값의 각도값을 구해준다.
+                m_Direction = m_EndPos - m_StartPos;
+                m_Direction.Normalize();
+                m_Rigid.gravityScale = 0;
+
+                //버튼 터치 업
+                if (Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    m_Rigid.gravityScale = 1;
+
+                    //애니메이션 움직이는거로 변경
+                    m_Anime.SetBool("b_Move", true);
+
+                    //플레이어 회전 초기화
+                    this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+                    //게임 속도 다시 원상태로 돌려주고
+                    GameManager.g_GameSpeed = 1.0f;
+
+                    //기존에 날라가던 velocity 힘을 0으로     
+                    m_Rigid.velocity = Vector2.zero;
+                    
+                    //그다음에 방향벡터에 이동속도를 곱해서 날라간다.
+                    //FixedDeltaTime으로 한 이유는 날라가는 속도를 일정하게 맞춰주기 위해
+                    Vector2 Power = m_Direction * MoveSpeed *100* Time.fixedDeltaTime;
+
+                    //AddForce로 velocity값을 증가시키면 무게의 영향을 받게되므로 직접 대입
+                    m_Rigid.velocity = Power;
+
+                    //플레이어 좌우 반전
+                    if (m_Rigid.velocity.x < 0)
+                    {
+                        m_Renderer.flipX = true;
+                    }
+                    else if (m_Rigid.velocity.x > 0)
+                    {
+                        m_Renderer.flipX = false;
+                    }
+
+                    //그다음 다시 땐상태로 enum값 되돌림
+                    m_Status = PlayerStatus.PS_Idle;
+                }
+                break;
+        }
+    }
+
+
+    public void OnTrigger(GameObject Child)
+    {
+        if (Child.gameObject.tag == "Player_Left")
+            m_CollDir = CollDir.CD_Left;
+        if (Child.gameObject.tag == "Player_Right")
+            m_CollDir = CollDir.CD_Right;
+        if (Child.gameObject.tag == "Player_Top")
+            m_CollDir = CollDir.CD_Top;
+        if (Child.gameObject.tag == "Player_Bottom")
+        {
+            m_CollDir = CollDir.CD_Bottom;
+            m_Anime.SetBool("b_Bottom", true);
+        }
+        else
+        {
+            m_Anime.SetBool("b_Bottom", false);
+        }
+    }
+}
